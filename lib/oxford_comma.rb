7cdb7f978e41064caac42d620c660c788d281050
costamay <titusouko@gmail.com>
2022-05-15 20:57:04 +0300

    commit tree 0eb5d597440586543ec7910bbd07781a95efda5f

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
+def oxford_comma(arr)
+    new_arr = ""
+    if arr.length < 3
+        new_arr = arr.join(" and ")
+    else
+        arr[-1] = "and #{arr[-1]}"
+        # arr.insert(-2, "and")
+        new_arr = arr.join(", ")
+    end
 
+    new_arr
 end
